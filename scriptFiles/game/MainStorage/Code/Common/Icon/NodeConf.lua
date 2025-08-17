46d146b7098bb35e872c73a9d4edf5a347b596d2
XF <1131680660@qq.com>
2025-08-17 18:08:19 +0800

    feat:比赛配置新增

--- scriptFiles/game/MainStorage/Code/Common/Icon/NodeConf.lua
+++ scriptFiles/game/MainStorage/Code/Common/Icon/NodeConf.lua
@@ -1,5 +1,8 @@
+---@class NodeConf
 local NodeConf = {
 	["倒计时"] = "Ground/init_map/terrain/Scene/UIRoot3D/计时器",
+	["科技岛倒计时"] = "Ground/map2/terrain/Scene/UIRoot3D/计时器",
+	["樱花岛倒计时"] = "Ground/map3/terrain/Scene/UIRoot3D/计时器",
 }
 
 return NodeConf
